9d197bb18ebd931ab3c9926a6aa8cf89847507fe
Shubhsharma3006 <11359shubhsharma@gmail.com>
2026-07-08 11:39:53 +0530

    basic questions ReverseArray

---
+++ src/Arrray/ReverseArray.java
@@ -0,0 +1,26 @@
+package Arrray;
+
+public class ReverseArray {
+    static void reverseArray(int[] arr){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        while (i <= j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int k : arr){
+            System.out.println(k);
+        }
+
+    }
+
+    static void main() {
+        int[] arr ={ 1, 2, 3, 4,5};
+        reverseArray(arr);
+    }
+}
+
